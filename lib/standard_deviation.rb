12e38b8ceefe1e2d8784264e02d5ca80905f77af
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-30 15:04:12 -0700

    Make small edits to standard deviation

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,41 +1,36 @@
 ages = [24, 30, 18, 20, 41]
 
-sum_ages = 0
+  sum_ages = ages.sum
+  num_ages = ages.length
+  average = sum_ages.to_f / num_ages.to_f
 
-ages.each do |num|
-  sum_ages += num
-end
+  step_4_ary = []
 
-num_ages = ages.length
+  step_4 = ages.each do |num|
+             step_4_ary << (num.to_f - average).round(2)
+           end
 
-average = sum_ages.to_f / num_ages.to_f
+  step_4_ary_squared = []
 
+  step_4_ary.each do |num|
+    step_4_ary_squared << (num ** 2).round(2)
+  end
 
-step_4_ary = []
+  step_5 = 0
 
-step_4 = ages.each do |num|
-           step_4_ary << (num.to_f - average).round(2)
-         end
+  step_4_ary_squared.each do |num|
+    step_5 += num
+  end
 
-step_4_ary_squared = []
+  step_6 = step_5.round(1)
 
-step_4_ary.each do |num|
-  step_4_ary_squared << (num ** 2).round(2)
-end
+  step_7 = step_6/ages.count.to_f
 
-step_5 = 0
+  standard_deviation = Math.sqrt(step_7).round(2)
 
-step_4_ary_squared.each do |num|
-  step_5 += num
-end
+  standard_deviation
 
-step_6 = step_5.round(1)
-
-step_7 = step_6/ages.count.to_f
-
-step_8 = Math.sqrt(step_7).round(2)
-
-
+require "Pry"; binding.pry
 
 # Your code here for calculating the standard deviation
 
