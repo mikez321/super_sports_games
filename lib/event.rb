6284d71aaa61898801e1334ad43be7f7426a5ed2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-30 14:55:59 -0700

    Recreate event class

--- lib/event.rb
+++ lib/event.rb
@@ -1,5 +1,3 @@
-require './lib/standard_deviation'
-
 class Event
   attr_reader :name, :ages
   def initialize(name, ages)
@@ -7,38 +5,33 @@ class Event
     @ages = ages
   end
 
-  def max_age(ages)
+  def max_age
     max_age = 0
     ages.each do |age|
       if age > max_age
         max_age = age
       end
     end
+    max_age
   end
 
-  def min_age(ages)
+  def min_age
     min_age = 200
     ages.each do |age|
       if age < min_age
         min_age = age
       end
     end
+    min_age
   end
 
-  def average_age(ages)
-    sums = 0
-    nums = ages.length.to_f
-    ages.each do |age|
-      age.to_f += average
-    end
-    average = sums / nums
+  def average_age
+    total_age = ages.sum.to_f
+    number_ages = ages.length.to_f
+    average = total_age / number_ages
     average
   end
 
-  def standard_deviation(ages)
-  end
-   
-
-
+  
 
 end
