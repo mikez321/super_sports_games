3b86fd6430025498e438cc5224d28d9b5ecb69c1
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-30 13:48:58 -0700

    Make rough looking but working standard deviation calculator

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,42 @@
 ages = [24, 30, 18, 20, 41]
 
+sum_ages = 0
+
+ages.each do |num|
+  sum_ages += num
+end
+
+num_ages = ages.length
+
+average = sum_ages.to_f / num_ages.to_f
+
+
+step_4_ary = []
+
+step_4 = ages.each do |num|
+           step_4_ary << (num.to_f - average).round(2)
+         end
+
+step_4_ary_squared = []
+
+step_4_ary.each do |num|
+  step_4_ary_squared << (num ** 2).round(2)
+end
+
+step_5 = 0
+
+step_4_ary_squared.each do |num|
+  step_5 += num
+end
+
+step_6 = step_5.round(1)
+
+step_7 = step_6/ages.count.to_f
+
+step_8 = Math.sqrt(step_7).round(2)
+
+
+
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
